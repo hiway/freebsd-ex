38f492230c6c23bb8eaf798bd1f8fa1b833d7c14
Harshad Sharma <harshad@sharma.io>
2022-04-15 20:32:37 +0530

    Add dializer and docs dependencies for development

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule FreeBSD.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.11", only: :dev},
+      {:earmark, "~> 0.1", only: :dev},
+      {:dialyxir, "~> 0.3", only: [:dev]},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
